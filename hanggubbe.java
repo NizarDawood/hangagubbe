159a86d116e55abfd58cec3e2bde7804811da42b
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-01-31 10:15:02 +0100

    scanner

---
+++ hanggubbe.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class hanggubbe {
+    public static void main(String[] args) {
+        try {
+            File ord = new File("filena");
+            Scanner fil = new Scanner(ord);
+           // String word = fil.nextLine();//
+           // System.out.println(word);//
+        //} catch (FileNotFoundException e) {
+            e.printStackTrace();//
+
+        string text = fil.nextLine();
+        String ord = "";
+        for (int i = 0; i < text.length(); i++) {
+    }
+}
+

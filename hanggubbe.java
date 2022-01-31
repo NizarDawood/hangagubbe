ab448044dc67c339c353eefede04ac6bd762150a
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-01-31 11:17:00 +0100

    Update hanggubbe.java

--- hanggubbe.java
+++ hanggubbe.java
@@ -3,17 +3,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class hanggubbe {
     public static void main(String[] args) {
-        try {
             File ord = new File("filena");
             Scanner fil = new Scanner(ord);
-           // String word = fil.nextLine();//
-           // System.out.println(word);//
-        //} catch (FileNotFoundException e) {
-            e.printStackTrace();//
+            String gissa = JOptionPane.showInputDialog("gissa på en bokstav");
 
-        string text = fil.nextLine();
-        String ord = "";
-        for (int i = 0; i < text.length(); i++) {
+
+
+            string text = fil.nextLine();
+
+            String ord = "";
+            for (int i = 0; i < text.length(); i++) {
+                char c = text.charAt(i);
+                if (c == gissa)
+                    ord += c;)
+            }
+        }
+        System.out.println(ord);
+
     }
 }
-

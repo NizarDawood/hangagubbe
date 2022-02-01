8d1a5ce8452865c68146ad4d54857d1e500e45ac
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-02-01 14:58:19 +0100

    Update hanggubbe.java
    testa om bokstaven sätmmer med ordet

--- hanggubbe.java
+++ hanggubbe.java
@@ -3,22 +3,20 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class hanggubbe {
     public static void main(String[] args) {
-            File ord = new File("filena");
+            File ord = new File("file");
             Scanner fil = new Scanner(ord);
             String gissa = JOptionPane.showInputDialog("gissa på en bokstav");
+        string text = fil.nextLine();
 
-
-
-            string text = fil.nextLine();
-
-            String ord = "";
-            for (int i = 0; i < text.length(); i++) {
-                char c = text.charAt(i);
+        String text = "_";
+            for (int i = 0; i < gissa.length(); i++) {
+                char c = gissa.charAt(i);
                 if (c == gissa)
-                    ord += c;)
+                    text += c;)
+                else( ':)' )
             }
         }
-        System.out.println(ord);
+        System.out.println(text);
 
     }
 }

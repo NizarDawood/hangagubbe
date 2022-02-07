1012cd0f68c4d9fb3e79ed369a0c0b4dfca5be6b
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-02-07 11:14:21 +0100

    Update hanggubbe.java

--- hanggubbe.java
+++ hanggubbe.java
@@ -1,22 +1,43 @@
+//liv: 5
+//input: X
+//ordet: .ete.
+//använda ored: bwp
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class hanggubbe {
     public static void main(String[] args) {
-            File ord = new File("file");
+        System.out.println(start);
+        try {
+            File ord = new File("filena");
             Scanner fil = new Scanner(ord);
-            String gissa = JOptionPane.showInputDialog("gissa på en bokstav");
-        string text = fil.nextLine();
-
-        String text = "_";
-            for (int i = 0; i < gissa.length(); i++) {
-                char c = gissa.charAt(i);
-                if (c == gissa)
-                    text += c;)
-                else( ':)' )
-            }
+            String word = fil.nextLine();
+            System.out.println(word);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        System.out.println(text);
+        string ordet = fil.nextLine();
+        char[] antalbokstäver = new char[ordet.length()];
+        for (int i = 0; i < ordet.length(); i++) {
+            ordet[i] = "_"
+        }
 
-    }
-}
+        int liv = 5;
+        while (liv > 0) {
+            System.out.println(liv:);
+            for (int i = 0; i < liv.length(); i++) {
+                System.out.println(O);
+
+            }
+
+            char ordet = input.charAt(0);
+            System.out.println("bokstaven:");
+            for (int i = 0; i < ordet.length(); i++) {
+                System.out.print(ordet[i]);
+            }
+            boolean gissningen = false
+            for (int i = 0; i < bokstav.length(); i++) {
+        char c = bokstav.charAt(i);
+        if (c == ordet){
+            ordet[i] = c;
+            gissningen = true}            
